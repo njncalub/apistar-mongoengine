6ecf53e68e4113448369c71fc68e1b813eac2d1b
Nap Joseph Calub <njncalub@gmail.com>
2018-05-17 01:47:12 +0800

    Moved parts to conftest and constants.

---
+++ tests/constants.py
@@ -0,0 +1,8 @@
+WORKING_HOST = '127.0.0.1'
+WORKING_PORT = 27017
+WORKING_DB = 'apistar_mongoengine_test_db'
+WORKING_ALIAS = 'default'
+NON_WORKING_PORT = 27641
+# controls how long the driver will wait to find an available, appropriate
+# server to carry out a database operation
+SERVER_SELECTION_TIMEOUT_MS = 5000
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,82 +1,16 @@
 import pytest
-
 from apistar import App, Route, TestClient, http
 from apistar_mongoengine.components import MongoClientComponent
 from pymongo.errors import ConnectionFailure
 
+from tests.constants import (
+    SERVER_SELECTION_TIMEOUT_MS,
+    WORKING_ALIAS,
+    WORKING_DB,
+    WORKING_HOST,
+    WORKING_PORT,
+)
 from tests.models import Todo
-from tests.utils import clear_connections_state
-
-
-WORKING_HOST = '127.0.0.1'
-WORKING_PORT = 27017
-WORKING_DB = 'apistar_mongoengine_test_db'
-WORKING_ALIAS = 'default'
-NON_WORKING_PORT = 27641
-# controls how long the driver will wait to find an available, appropriate
-# server to carry out a database operation
-SERVER_SELECTION_TIMEOUT_MS = 5000
-
-
-@pytest.fixture
-def sconn_params():
-    """
-    Simple connection component using different parametrs.
-    """
-    clear_connections_state()
-
-    options = {
-        'host': WORKING_HOST,
-        'port': WORKING_PORT,
-        'name': WORKING_DB,
-        'alias': WORKING_ALIAS,
-        'serverSelectionTimeoutMS': SERVER_SELECTION_TIMEOUT_MS,
-    }
-    return MongoClientComponent(**options)
-
-
-@pytest.fixture
-def sconn_host():
-    """
-    Simple connection component using a host string.
-    """
-    clear_connections_state()
-
-    options = {
-        'host': f'mongodb://{WORKING_HOST}:{WORKING_PORT}/{WORKING_DB}',
-        'serverSelectionTimeoutMS': SERVER_SELECTION_TIMEOUT_MS,
-    }
-    return MongoClientComponent(**options)
-
-
-@pytest.fixture
-def sconn_mockhost():
-    """
-    Simple connection component using a mock host string.
-    """
-    clear_connections_state()
-
-    options = {
-        'host': f'mongomock://{WORKING_HOST}:{WORKING_PORT}/{WORKING_DB}',
-        'serverSelectionTimeoutMS': SERVER_SELECTION_TIMEOUT_MS,
-    }
-    return MongoClientComponent(**options)
-
-
-@pytest.fixture
-def sconn_invalid_port():
-    """
-    Simple connection component using an invalid port number.
-    """
-    clear_connections_state()
-
-    options = {
-        'host': WORKING_HOST,
-        'port': NON_WORKING_PORT,
-        'name': WORKING_DB,
-        'serverSelectionTimeoutMS': SERVER_SELECTION_TIMEOUT_MS,
-    }
-    return MongoClientComponent(**options)
 
 
 def test_simple_connect_using_params(sconn_params):
